ef08726e9fddcbaa25fa1b1f01d88787f4844047
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-02 19:04:13 +0545

    simple api

--- rusre-backend/src/main.rs
+++ rusre-backend/src/main.rs
@@ -1,15 +1,30 @@
-use actix_web::get;
-use actix_web::{self, HttpResponse, HttpServer};
+use actix_web::{self, HttpResponse, HttpServer, post};
 use actix_web::{App, Responder};
+use actix_web::{get, web};
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct ReturnData {
+    name: String,
+    message: String,
+}
 
 #[get("/")]
 pub async fn hello() -> impl Responder {
     HttpResponse::Ok().body("hello santosh")
 }
+#[post("/api/json")]
+pub async fn json_response() -> impl Responder {
+    let data = ReturnData {
+        name: "Santosh".to_string(),
+        message: "Hello there what is going on".to_string(),
+    };
+    web::Json(data)
+}
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().service(hello))
+    HttpServer::new(|| App::new().service(hello).service(json_response))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
