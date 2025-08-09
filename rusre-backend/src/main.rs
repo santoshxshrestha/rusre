a389544632b89d72417b9a981eb7fefc18bac2fc
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-09 15:21:17 +0545

    ip should be 0.0.0.0

--- rusre-backend/src/main.rs
+++ rusre-backend/src/main.rs
@@ -45,7 +45,7 @@ async fn main() -> Result<(), std::io::Error> {
             .service(random)
             .service(hello)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
