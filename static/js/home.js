10ddf186a6a51d8b936c883930d181ccc93ab4e8
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-10 16:38:10 +0545

    added better ui

--- static/js/home.js
+++ static/js/home.js
@@ -2,7 +2,7 @@ const nextButton = document.getElementsByClassName("next");
 const quote = document.getElementsByClassName("quote");
 const author = document.getElementsByClassName("author");
 
-nextButton[0].addEventListener("click", async () => {
+async function fatchRandomQuote() {
   try {
     const response = await fetch("/quote/random");
     if (!response.ok) throw new Error("Network response was not ok");
@@ -12,4 +12,10 @@ nextButton[0].addEventListener("click", async () => {
   } catch (error) {
     console.error("Failed to fetch quote:", error);
   }
-});
+}
+
+// run on button click
+nextButton[0].addEventListener("click", fatchRandomQuote());
+
+// run once teh page loads he he
+window.addEventListener("load", fatchRandomQuote());
